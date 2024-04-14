9857fe6679abeaab76a181fbe25fc60ba9ec186f
Agustin Lascar <aguslascar@hotmail.com.ar>
2024-04-14 00:20:58 -0300

    operated volume model classes

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/model/Asset.kt
@@ -0,0 +1,8 @@
+package ar.edu.unq.desapp.grupoI.backenddesappapi.model
+
+class Asset (
+    cryptoAsset: CryptoCurrency,
+    nominalAmount: Double,
+    actualPrice: Double,
+    amountInPesos: Double
+)
---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/model/OperatedVolume.kt
@@ -0,0 +1,8 @@
+package ar.edu.unq.desapp.grupoI.backenddesappapi.model
+
+class OperatedVolume (
+    requestDate: String,
+    totalAmountUSD: Double,
+    totalAmountPesos: Double,
+    assets: List<Asset>
+)
