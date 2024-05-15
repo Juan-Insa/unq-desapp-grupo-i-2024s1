66c04380392fc83f317283de10949a43fcdbe342
Agustin Lascar <aguslascar@hotmail.com.ar>
2024-05-15 13:02:47 -0300

    test data at start of app

--- src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/BackendDesappApiApplication.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/BackendDesappApiApplication.kt
@@ -1,7 +1,11 @@
 package ar.edu.unq.desapp.grupoI.backenddesappapi
 
+import ar.edu.unq.desapp.grupoI.backenddesappapi.utils.DataService
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -14,5 +18,11 @@ class BackendDesappApiApplication {
 			SpringApplication.run(BackendDesappApiApplication::class.java, *args)
 		}
 	}
+	@Component
+	class DataLoader(private val dataService: DataService) : ApplicationRunner {
 
+		override fun run(args: ApplicationArguments?) {
+			dataService.createTestData()
+		}
+	}
 }
