9e855fe4153996de8889d4f89f2729acaba49fdd
Juan Cruz Insaurralde <juancruzinsaurralde@outlook.com>
2024-06-23 18:18:12 -0300

    update on logging aspect to log the authenticated user

--- src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/aspect/LoggingAspect.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/aspect/LoggingAspect.kt
@@ -7,6 +7,7 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
 @Aspect
@@ -19,18 +20,17 @@ class LoggingAspect {
     fun logWebServiceCalls(joinPoint: ProceedingJoinPoint): Any? {
         val startTime = System.currentTimeMillis()
 
-        // Obtener los detalles del método y los parámetros
         val methodName = joinPoint.signature.toShortString()
         val params = joinPoint.args.joinToString()
-        val user = getUserIdFromArgs(joinPoint.args)
 
-        // Ejecutar el método
+        val authentication = SecurityContextHolder.getContext().authentication
+        val user = authentication.name
+
         val result = joinPoint.proceed()
 
         val elapsedTime = System.currentTimeMillis() - startTime
         val timestamp = java.time.Instant.ofEpochMilli(startTime).toString()
 
-        // Registrar la información de auditoría
         logger.info("timestamp=$timestamp, user=$user, method=$methodName, params=$params, elapsedTime=${elapsedTime}ms")
 
         return result
