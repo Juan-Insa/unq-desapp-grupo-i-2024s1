455ae3e4ac3d00963f9d1e9e1f4cbb6316974853
Agustin Lascar <aguslascar@hotmail.com.ar>
2024-06-26 11:33:57 -0300

    transactions integration test

--- src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/webservice/controllers/dto/TransactionDTO.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/webservice/controllers/dto/TransactionDTO.kt
@@ -6,6 +6,7 @@ import ar.edu.unq.desapp.grupoI.backenddesappapi.model.enums.OperationState
 import java.time.LocalDateTime
 
 class TransactionDTO(
+    val id: Long,
     val interestedUserEmail: String,
     val action: Action,
     val initTime: LocalDateTime,
@@ -16,6 +17,7 @@ class TransactionDTO(
     companion object {
         fun fromModel(transaction: Transaction): TransactionDTO {
             return TransactionDTO(
+                id = transaction.id!!,
                 interestedUserEmail = transaction.interestedUser.email,
                 action = transaction.action,
                 initTime = transaction.initTime,
---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoI/backenddesappapi/webservice/controllers/IntentionControllerTest.kt
@@ -0,0 +1,178 @@
+package ar.edu.unq.desapp.grupoI.backenddesappapi.webservice.controllers
+
+import ar.edu.unq.desapp.grupoI.backenddesappapi.model.CryptoCurrency
+import ar.edu.unq.desapp.grupoI.backenddesappapi.model.Intention
+import ar.edu.unq.desapp.grupoI.backenddesappapi.model.User
+import ar.edu.unq.desapp.grupoI.backenddesappapi.model.enums.Asset
+import ar.edu.unq.desapp.grupoI.backenddesappapi.model.enums.Operation
+import ar.edu.unq.desapp.grupoI.backenddesappapi.persistence.repository.IntentionRepository
+import ar.edu.unq.desapp.grupoI.backenddesappapi.persistence.repository.TransactionRepository
+import ar.edu.unq.desapp.grupoI.backenddesappapi.persistence.repository.UserRepository
+import ar.edu.unq.desapp.grupoI.backenddesappapi.service.BinanceProxyService
+import ar.edu.unq.desapp.grupoI.backenddesappapi.webservice.controllers.dto.LoginUserDTO
+import ar.edu.unq.desapp.grupoI.backenddesappapi.webservice.controllers.request.IntentionRequest
+import ar.edu.unq.desapp.grupoI.backenddesappapi.webservice.controllers.request.UserRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import org.mockito.Mockito
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.ResultActions
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = ["classpath:application-integrationtest.properties"])
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class IntentionControllerTest {
+
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+
+    @Autowired
+    private lateinit var intentionRepository: IntentionRepository
+
+    @Autowired
+    private lateinit var transactionRepository: TransactionRepository
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var authenticationController: AuthenticationController
+
+    @MockBean
+    private lateinit var binanceProxyService: BinanceProxyService
+
+    lateinit var testSellUser: User
+    lateinit var testBuyUser: User
+    lateinit var tokenSell: String
+    lateinit var tokenBuy: String
+
+    @BeforeEach
+    fun cleanUp() {
+        transactionRepository.deleteAll()
+        intentionRepository.deleteAll()
+    }
+    @BeforeAll
+    fun setup() {
+        testSellUser = createUserAndRegister("testSellUser")
+        testBuyUser = createUserAndRegister("testBuyUser")
+        tokenSell = authenticationController.authenticate(LoginUserDTO(testSellUser.email, testSellUser.password)).body?.token!!
+        tokenBuy = authenticationController.authenticate(LoginUserDTO(testBuyUser.email, testBuyUser.password)).body?.token!!
+    }
+    @Test
+    fun whenPostNewIntention_thenStatus200() {
+        createTestIntentionBy(testSellUser, Operation.SELL, tokenSell)
+            .andExpect(status().isOk)
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.operation", `is`(Operation.SELL.name)))
+    }
+
+    @Test
+    fun whenGetAllIntentions_thenStatus200() {
+        createTestIntentionBy(testBuyUser, Operation.BUY, tokenBuy)
+        createTestIntentionBy(testSellUser, Operation.SELL, tokenSell)
+
+        mvc.perform(
+            get("/api/intention/all")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer $tokenSell")
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$[0].operation", `is`(Operation.BUY.name)))
+            .andExpect(jsonPath("$[1].operation", `is`(Operation.SELL.name)))
+    }
+
+    @Test
+    fun whenGetAllActiveIntentions_thenStatus200() {
+        createTestIntentionBy(testBuyUser, Operation.BUY, tokenBuy)
+        createTestIntentionBy(testSellUser, Operation.SELL, tokenSell)
+
+        mvc.perform(
+            get("/api/intention/active")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer $tokenSell")
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$[0].operation", `is`(Operation.BUY.name)))
+            .andExpect(jsonPath("$[1].operation", `is`(Operation.SELL.name)))
+    }
+
+    @Test
+    fun whenGetActiveIntentionsByUserId_thenStatus200() {
+        createTestIntentionBy(testBuyUser, Operation.BUY, tokenBuy)
+        createTestIntentionBy(testBuyUser, Operation.SELL, tokenBuy)
+        val userId = testBuyUser.id!!
+        mvc.perform(
+            get("/api/intention/user/$userId/active")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer $tokenBuy")
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$[0].operation", `is`(Operation.BUY.name)))
+            .andExpect(jsonPath("$[1].operation", `is`(Operation.SELL.name)))
+    }
+
+
+    fun createTestIntentionBy(user: User, operation: Operation, token: String): ResultActions {
+        val intention = Intention(
+            user = user,
+            operation = operation,
+            cryptoAsset = Asset.BTCUSDT,
+            amount = 10.0,
+            price = 60000.00
+        )
+        val intentionRequest = IntentionRequest(
+            intention = intention,
+            userId = user.id!!
+        )
+        val resultadoMockeado = CryptoCurrency("BTCUSDT", 60000.0f, "2022-06-25 12:00:00")
+
+        Mockito.`when`(binanceProxyService.getCryptoCurrency("BTCUSDT")).thenReturn(resultadoMockeado)
+        return mvc.perform(
+            post("/api/intention/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(intentionRequest))
+                .header("Authorization", "Bearer $token")
+        )
+    }
+
+    fun createUserAndRegister(name: String): User {
+        var intentionUser = User(
+            name = name,
+            lastName = "user",
+            email = "$name@gmail.com",
+            address = "intentionUserAddress",
+            password = "Intention.User.Pass",
+            cvu = "1234567890123456789012",
+            cryptoWalletAddress = "12345678")
+        val registeredUser = authenticationController.registerUser(UserRequest(intentionUser)).body!!
+        intentionUser.id = registeredUser.id
+        return intentionUser
+    }
+    @AfterAll
+    fun clean() {
+        transactionRepository.deleteAll()
+        intentionRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+}
